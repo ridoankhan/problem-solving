a0963e2d7e22c0ce0d6a8e4a159c5a67390cbcdd
Ridoan Khan Anik <ridoan.anik@bjitgroup.com>
2023-03-30 15:51:01 +0600

    valid parentheisis added

--- 150/04.validParenthesis.js
+++ 150/04.validParenthesis.js
@@ -1,7 +1,45 @@
 // problem no: 20
 
-const isValid = (str) => {}
+const isValidWithStack = (str) => {
+  let stack = []
+  let openingBrackets = ['(', '{', '[']
+  let closingBrackets = [')', '}', ']']
 
-const input = '()[]{}'
+  for (let i = 0; i < str.length; i++) {
+    if (openingBrackets.includes(str[i])) {
+      stack.push(str[i])
+    } else if (closingBrackets.includes(str[i])) {
+      let expectedBracket = openingBrackets[closingBrackets.indexOf(str[i])]
+      if (stack.length === 0 || stack.pop() !== expectedBracket) {
+        return false
+      }
+    }
+  }
+  return stack.length === 0
+}
 
-console.log(isValid(input))
+var isValidWithMap = function (s) {
+  let stack = []
+  let map = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i]
+    if (map[char]) {
+      stack.push(map[char])
+    } else if (stack.length === 0 || char !== stack.pop()) {
+      return false
+    }
+  }
+
+  return stack.length === 0
+}
+
+const s = '()'
+// const s = "()[]{}"
+// const s = "(]"
+
+console.log(isValid(s))
--- 150/06.removeDuplicates.js
+++ 150/06.removeDuplicates.js
@@ -13,14 +13,4 @@ var countUniqueNumbers = function (nums) {
 const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 // const nums = [1,1,2]
 
-const countUnique = (nums) => {
-  let count = 0
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === nums[i + 1]) {
-      count++
-    }
-  }
-  return count
-}
-
 console.log(countUnique(nums))
